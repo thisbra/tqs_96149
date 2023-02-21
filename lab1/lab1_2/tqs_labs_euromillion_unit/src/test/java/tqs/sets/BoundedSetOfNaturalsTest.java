d15a77265c0aa61c2f1d1a0c3c7d2ff6ab9f9281
thisbra <thiagosbrasil@ua.pt>
2023-02-21 16:51:53 +0000

    lab1_2

---
+++ lab1/lab1_2/tqs_labs_euromillion_unit/src/test/java/tqs/sets/BoundedSetOfNaturalsTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tqs.sets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+import tqs.sets.BoundedSetOfNaturals;
+
+/**
+ * @author ico0
+ */
+class BoundedSetOfNaturalsTest {
+    private BoundedSetOfNaturals setA;
+    private BoundedSetOfNaturals setB;
+    private BoundedSetOfNaturals setC;
+
+
+    @BeforeEach
+    public void setUp() {
+        setA = new BoundedSetOfNaturals(1);
+        setB = BoundedSetOfNaturals.fromArray(new int[]{10, 20, 30, 40, 50, 60});
+        setC = BoundedSetOfNaturals.fromArray(new int[]{50, 60});
+    }
+
+    @AfterEach
+    public void tearDown() {
+        setA = setB = setC = null;
+    }
+
+//    @Disabled("TODO revise test logic")
+    @Test
+    public void testAddElement() {
+
+        setA.add(99);
+        assertTrue(setA.contains(99), "add: added element not found in set.");
+        assertEquals(1, setA.size());
+
+        // Checks if set Dip is full
+        assertThrows(IllegalArgumentException.class, () -> setB.add(11), "add: added elements on full sett");
+    }
+
+//    @Disabled("TODO revise to test the construction from invalid arrays")
+    @Test
+    public void testAddFromBadArray() {
+        int[] elems = new int[]{10, -20, -30};
+
+        // must fail with exception
+        assertThrows(IllegalArgumentException.class, () -> setA.add(elems));
+    }
+
+    @Test
+    public void testIntersection() {
+        assertFalse(setB.intersects(setA), "Reported unexisting intersection");
+
+        assertTrue(setB.intersects(setC), "Did not reported existing intersection");
+    }
+
+
+}
