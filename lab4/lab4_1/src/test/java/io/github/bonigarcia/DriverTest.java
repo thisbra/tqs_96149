844dede0703bcc5801f9f353f2594b19a23893ed
thisbra <thiagosbrasil@ua.pt>
2023-03-12 12:50:11 +0000

    lab4_2 b)

--- lab4/lab4_1/src/test/java/io/github/bonigarcia/DriverTest.java
+++ lab4/lab4_1/src/test/java/io/github/bonigarcia/DriverTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.slf4j.Logger;
@@ -33,7 +36,7 @@ public class DriverTest {
     }
 
     @Test
-    void test() {
+    void testFirst() {
         String setUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
 
         driver.get(setUrl);
@@ -44,6 +47,66 @@ public class DriverTest {
 
         assertEquals("Hands-On Selenium WebDriver with Java", title);
 
+    }
+
+    @Test
+    public void blazefirst() {
+        driver.get("https://blazedemo.com/");
+        driver.manage().window().setSize(new Dimension(1314, 741));
+
+        String title1 = driver.getTitle();
+        assertEquals("BlazeDemo", title1, "Home Page title did not match expected");
+
+        driver.findElement(By.name("fromPort")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("fromPort"));
+            dropdown.findElement(By.xpath("//option[. = 'São Paolo']")).click();
+        }
+        driver.findElement(By.cssSelector(".form-inline:nth-child(1) > option:nth-child(7)")).click();
+        driver.findElement(By.name("toPort")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("toPort"));
+            dropdown.findElement(By.xpath("//option[. = 'New York']")).click();
+        }
+        driver.findElement(By.cssSelector(".form-inline:nth-child(4) > option:nth-child(5)")).click();
+        driver.findElement(By.cssSelector(".btn-primary")).click();
+
+        String title2 = driver.getTitle();
+        assertEquals("BlazeDemo - reserve", title2, "Reserve Page title did not match expected");
+
+        driver.findElement(By.cssSelector("tr:nth-child(2) .btn")).click();
+
+        String title3 = driver.getTitle();
+        assertEquals("BlazeDemo Purchase", title3, "Purchase Page title did not match expected");
+
+        driver.findElement(By.id("inputName")).click();
+        driver.findElement(By.id("inputName")).sendKeys("Thiago S. Brasil");
+        driver.findElement(By.id("address")).sendKeys("Rua do Loureiro, 4");
+        driver.findElement(By.id("city")).click();
+        driver.findElement(By.id("city")).sendKeys("Recife");
+        driver.findElement(By.id("state")).click();
+        driver.findElement(By.id("state")).sendKeys("Pernambuco");
+        driver.findElement(By.id("zipCode")).click();
+        driver.findElement(By.id("zipCode")).sendKeys("3810-187");
+        driver.findElement(By.id("cardType")).click();
+        {
+            WebElement dropdown = driver.findElement(By.id("cardType"));
+            dropdown.findElement(By.xpath("//option[. = 'American Express']")).click();
+        }
+        driver.findElement(By.cssSelector("option:nth-child(2)")).click();
+        driver.findElement(By.id("creditCardNumber")).click();
+        driver.findElement(By.id("creditCardNumber")).sendKeys("78787878787878");
+        driver.findElement(By.id("creditCardMonth")).click();
+        driver.findElement(By.id("creditCardMonth")).sendKeys("02");
+        driver.findElement(By.id("creditCardYear")).click();
+        driver.findElement(By.id("creditCardYear")).sendKeys("2023");
+        driver.findElement(By.id("nameOnCard")).click();
+        driver.findElement(By.id("nameOnCard")).sendKeys("Thiago S. Brasil");
+        driver.findElement(By.cssSelector(".checkbox")).click();
+        driver.findElement(By.cssSelector(".btn-primary")).click();
+
+        String title = driver.getTitle();
+        assertEquals("BlazeDemo Confirmation", title, "Confirmation Page title did not match expected");
     }
 
     @AfterEach
